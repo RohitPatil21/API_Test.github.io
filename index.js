2f6987aa9d8cecf0b41c2dbd8cf42a561b1d895a
Rohit <rohitpatil7@yahoo.com>
2021-12-12 19:25:07 +0530

    Add Design

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 async function fnFetch(){
     var res =  await fetch( "https://meme-api.herokuapp.com/gimme")
     var data = await res.json();
-    document.getElementById('image').src = data.preview[3]
-    console.log(data.preview[3]);
+    document.getElementById('image').src = data.preview[2];
+    document.getElementById('title').innerText = data.subreddit;
+    document.getElementById('postlink').innerText = data.postLink;
+    console.log(data);
+    console.log(data.subreddit)
 }
